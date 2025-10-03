88d17c282bf6dbb502d321e46ba055b937c80844
Abdulrahman A. Alkhatib <69086489+AbdulrahmanAKh@users.noreply.github.com>
2025-10-04 00:51:30 +0300

    Update CategorySection.tsx

--- src/components/Home/CategorySection.tsx
+++ src/components/Home/CategorySection.tsx
@@ -416,7 +416,7 @@ const CategorySection = () => {
                   const IconComponent = getIconComponent(category.icon_name, category.name_ar);
                   
                   return (
-                    <CarouselItem key={category.id} className="md:basis-1/12 lg:basis-1/12">
+                    <CarouselItem key={category.id} className="md:basis-1/2 lg:basis-4/12">
                       <Link to={`/explore?category=${category.id}`}>
                         <Card className="group overflow-hidden hover:shadow-xl smooth-transition">
                           <div className="relative h-64">
